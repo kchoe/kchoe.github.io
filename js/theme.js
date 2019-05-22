93290addaed81553b1e95815a1f938db24cc1cdc
Kukrim Choe <kukrimchoe@Kukrims-MacBook-Pro.local>
2019-05-22 01:10:01 -0700

    All requirements implemented

---
+++ js/theme.js
@@ -0,0 +1,12 @@
+
+    // Open/Closes the sidebar menu
+
+    function menuClose(){
+        var menu = document.getElementById("sidebar-wrapper");
+        menu.className = menu.className.replace(" active", "");
+    }
+
+    function menuOpen(){
+        var menu = document.getElementById("sidebar-wrapper");
+        menu.className += " active";
+    }
